e59d795a652552bacdf5fb11e4eb313c9b7990cf
BATMAN <b.atman18677@gmail.com>
2026-06-13 13:55:35 +0530

    Update security.js

--- backend/src/middleware/security.js
+++ backend/src/middleware/security.js
@@ -1,12 +1,20 @@
 const helmet = require('helmet');
 const cors = require('cors');
+const config = require('../config');
+
+const allowedOrigins = config.ALLOWED_ORIGINS === '*'
+  ? '*'
+  : config.ALLOWED_ORIGINS.split(',').map((s) => s.trim());
 
 const security = [
   helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
   }),
-  cors(),
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
 ];
 
 module.exports = security;
